4248b4878bf8018450ef6d4885e134822b6bb914
kibambe-0167 <kibambe.0167@gmail.com>
2023-04-29 22:22:45 +0200

    added form

--- article.md
+++ article.md
@@ -44,4 +44,17 @@ I usually delete the entire codes in file of src/App.js
 ### let create a slice.
 - you might get errors about the redux created in src/redux_/store.js not having valid reducers, ignore that for now, because in our next step we're going to fix that.
 - now to create a slice, which will provider reducers to our react redux store. we need to make a file, you might prefer to put all you slices in one file, i just prefer to put mine in different files.
-- i will create a file named, counter in src/redux_/slices/counter.js
+- i will create a file named, tasks in src/redux_/slices/tasks.js
+- will import createSlice from @reduxjs/toolkit.
+- usually create my initialState at the top of the file.
+- will create an object of createSlice. give it a name. provide it initialState and provide an object to hold all its actions, reducers. see file src/redux_/slice/tasks.js
+- we will have 3 methods or actions for this article, one to add tasks, another to delete a task and another to edit a task.
+- we will keep an array to hold all our tasks objects.
+- i hope this code in the reducers/actions is familiar.
+- we will export all our actions so that we can letter use them.
+- we will also export a method to access all our tasks in the store.
+- we'll export our reducer for the redux store we created in src/redux_/store.js, so that our store can be registered globally in our app.
+
+
+### create forms and methods to access the tasks.
+- 
--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
+  function handleSubmit(event) {
+    event.preventDefault();
+    console.log("gvjhbjnkml,");
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1 className=" text-5xl text-slate-300 ">ReactJS Redux</h1>
       </header>
+
+      <div className=" w-full flex flex-row justify-between px-5 py-5 ">
+        <form
+          onSubmit={(event) => handleSubmit(event)}
+          className=" bg-red-700 w-4/12 px-1 "
+        >
+          <div className=" my-3 w-full ">
+            <input
+              className=" w-full rounded-sm px-2 py-2 color text-lg "
+              placeholder="Tasks name"
+            />
+          </div>
+
+          <div className=" my-3">
+            <button className=" bg-green-900 w-full py-2 rounded-lg text-white ">
+              Add Task
+            </button>
+          </div>
+        </form>
+        <div className=" bg-green-700 w-4/12 mx-3 ">all</div>
+        <div className=" bg-blue-700 w-4/12 ">edit</div>
+      </div>
     </div>
   );
 }
